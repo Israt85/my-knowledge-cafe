f6300e73f8850010e434d3727a5d56e0d73006ae
Israt85 <isratanjuman3@gmail.com>
2023-09-14 12:55:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './component/blogs/blogs'
+import Header from './component/header/header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
